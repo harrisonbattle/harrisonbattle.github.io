43b48841acfefbdf7ca03eb28ff0412f55d47d9d
harrisonsbattle@gmail.com <harrisonsbattle@gmail.com>
2025-05-02 13:51:29 -0700

    Navagation Bar Movement

--- JS/functions.js
+++ JS/functions.js
@@ -8,8 +8,17 @@ function showSection(sectionId) {
     if (sectionToShow) {
         sectionToShow.style.display = 'block';
     }
+
+    const nav = document.querySelector('nav');
+    if (nav) {
+        if (sectionId === 'home') {
+            nav.classList.remove('scrolled');
+        } else {
+            nav.classList.add('scrolled');
+        }
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     showSection('home');
-})
+});
